4450550ed225d4676a3af1e21886562cdd97d41e
Arie1906 <nttispay@gmail.com>
2025-08-16 14:35:26 +0700

    upload homepage

---
+++ test/admin.go
@@ -0,0 +1,10 @@
+package service
+
+//// AdminService defines administration operations
+//type AdminService interface {
+//	GetAllUsers() ([]core.User, error)
+//	GetReportedPosts() ([]Report, error)
+//	ManageUser(userID string, action string) error
+//	ManagePost(postID string, action string) error
+//	ManageCategories(action string, category *Category) error
+//}
